0c22d94029758fc7614d9908c1663da621516c3d
Michał Purchla <michalpurchla@gmail.com>
2025-07-08 17:28:15 +0200

    Entrypoint.sh in LF format

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,15 @@
-#!/bin/bashecho "Waiting for DB..."sleep 5python manage.py migratepython manage.py shell -c "from django.contrib.auth import get_user_modelUser = get_user_model()if not User.objects.filter(username='admin').exists():    User.objects.create_superuser('admin', 'admin@example.com', 'admin')"exec "$@"
+#!/bin/bash
+
+echo "Waiting for DB..."
+sleep 5
+
+python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+"
+
+exec "$@"
